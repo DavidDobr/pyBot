66add155bc0b9d0d94e29576cd841795a734cae4
David Dobrinskiy <david@dobrinskiy.com>
2017-04-19 23:05:11 +0000

    echo user's messages

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 #!/home/adam/miniconda3/envs/tBot/bin/python
 
-print('before TG import')
+# import https://github.com/python-telegram-bot
+# you can use: conda install -c daviddobr python-telegram-bot=5.3.0
+# urllib3 can be installed using: conda install -c daviddobr urllib3=1.20
+
 import telegram
-print('after TG import')
 
 with open('tars.token', 'r') as f:
     tarsToken = f.readlines()[0].replace('\n', '')
@@ -28,7 +30,14 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+# Let's add another handler that listens for regular messages.
+# Use the MessageHandler, another Handler subclass, to echo to all text messages:
+def echo(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
 
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 
 print('starting bot now...')
 updater.start_polling()
